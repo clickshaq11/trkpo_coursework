d0404784ad1b25eed60d0f0f8e8157eae44314b9
Dmitry Prudnikov <dmitry.prudnikov@sredasolutions.com>
2023-11-11 15:15:02 +0300

    feat(52): add update and delete post api FS Post_4-10, HLD 7.3.6, 7.3.7

--- backend/src/main/java/com/trkpo/controller/PostController.java
+++ backend/src/main/java/com/trkpo/controller/PostController.java
@@ -1,12 +1,18 @@
 package com.trkpo.controller;
 
+import com.trkpo.model.dto.request.UpdatePostDto;
 import com.trkpo.model.dto.response.MyPostDto;
 import com.trkpo.service.PostService;
 import java.security.Principal;
 import java.util.List;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +25,15 @@ public class PostController {
     @GetMapping("/filter/mine")
     public List<MyPostDto> getMine(Principal principal, Pageable pageable){
         return service.getMine(principal.getName(), pageable);
+    }
+
+    @PutMapping("/{id}")
+    public void updateById(Principal principal, @RequestBody @Valid UpdatePostDto dto, @PathVariable Integer id) {
+        service.updateById(principal.getName(), dto, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(Principal principal, @PathVariable Integer id) {
+        service.deleteById(principal.getName(), id);
     }
 }
