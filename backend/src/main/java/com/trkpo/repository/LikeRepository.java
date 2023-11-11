03a389f06863ccca3236e0d30151f90b0db6744f
Dmitry Prudnikov <dmitry.prudnikov@sredasolutions.com>
2023-11-11 13:58:42 +0300

    feat(25): add like api FS My_Profile_24-25 Other_Profile_17-18 Post_12-13 News_Feed_5-6 and HLD 7.6.1

--- backend/src/main/java/com/trkpo/repository/LikeRepository.java
+++ backend/src/main/java/com/trkpo/repository/LikeRepository.java
@@ -1,8 +1,11 @@
 package com.trkpo.repository;
 
 import com.trkpo.model.entity.LikeEntity;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LikeRepository extends JpaRepository<LikeEntity, Integer> {
     Boolean existsByPostId(Integer postId);
+
+    Optional<LikeEntity> findByUserIdAndPostId(Integer userId, Integer postId);
 }
