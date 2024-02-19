d3363f7285a868717ecf313cc2ea81372bcff711
forlorn <clickshaq11@gmail.com>
2024-02-19 21:20:56 +0300

    change test suite name and remove int tests from units

--- frontend/vitest.config.unit.ts
+++ frontend/vitest.config.unit.ts
@@ -5,15 +5,20 @@ export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
+      include: [
+        'src/**/*.test.ts',
+        'src/**/*.test.tsx',
+        '!src/test/integration/*'
+      ],
       globals: true,
       environment: 'jsdom',
       setupFiles: [
         'src/test/unit/mockEndpoints.ts',
         'src/test/unit/QueryProviderTestWrapper.tsx',
         'src/test/unit/mockLocalStorage.ts',
-        'src/test/unit/setupTests.ts',
+        'src/test/unit/setupTests.ts'
       ],
-      reporters: process.env.GITHUB_ACTIONS ? ['default', 'github-actions'] : ['default'],
-    },
-  }),
+      reporters: process.env.GITHUB_ACTIONS ? ['default', 'github-actions'] : ['default']
+    }
+  })
 );
